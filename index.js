282c2b1bb19826fe4e5845bd477ecba422ff1622
itadmin <itadmin@lapr101.successivetech.com>
2019-10-20 16:02:42 +0530

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const EventSource = require('eventsource');
 const WebhooksApi = require('@octokit/webhooks')
 const Octokit = require("@octokit/rest");
+const request = require('request')
 
 const octokit =  new Octokit({ auth: { username: "octocat", password: "secret"}});
 
@@ -22,4 +23,16 @@ source.onmessage = (event) => {
 
 webhooks.on('check_suite', async ({ id, name, payload }) => {
     console.log(name, 'event received')
+    let head_sha = payload.check_suite.head_sha
+    create_check_run(head_sha);
+})
+
+const create_check_run = (head_sha) => request('POST /repos/:owner/:repo/check-runs', {
+    accept: 'application/vnd.github.antiope-preview+json',
+    name: 'Audit Check',
+    head_sha: head_sha
+})
+
+webhooks.on('check_run', async({ id, name, payload }) => {
+    console.log(name, 'event recvddddd')
 })
