88ab03386268d070c4a3624ecd0e9843fadd839d
itadmin <itadmin@lapr101.successivetech.com>
2019-10-21 01:30:15 +0530

    index.js

--- index.js
+++ index.js
@@ -4,41 +4,41 @@ const Octokit = require("@octokit/rest")
 const request = require("@octokit/request")
 const { App } = require("@octokit/app")
 
-const app = new App({ 
-    id: process.env.GITHUB_APP_IDENTIFIER, 
-    privateKey: process.env.GITHUB_PRIVATE_KEY 
-});
+// const app = new App({ 
+//     id: process.env.GITHUB_APP_IDENTIFIER, 
+//     privateKey: process.env.GITHUB_PRIVATE_KEY 
+// });
 
-const getInstallationAccessTokenByInstallationId = async (installationId) => {
-    const installationAccessToken = await app.getInstallationAccessToken({
-        installationId,
-    });
-    return installationAccessToken;
-};
+// const getInstallationAccessTokenByInstallationId = async (installationId) => {
+//     const installationAccessToken = await app.getInstallationAccessToken({
+//         installationId,
+//     });
+//     return installationAccessToken;
+// };
 
-const getInstallationAccessToken = async (owner, repo) => {
-    const { data } = await request(`https://api.github.com/repos/${owner}/${repo}/installation`,
-    {
-        headers: {
-        authorization: `Bearer ${app.getSignedJsonWebToken()}`,
-        accept: 'application/vnd.github.machine-man-preview+json',
-    },
-});
+// const getInstallationAccessToken = async (owner, repo) => {
+//     const { data } = await request(`GET /repos/${owner}/${repo}/installation`,
+//     {
+//         headers: {
+//         authorization: `Bearer ${app.getSignedJsonWebToken()}`,
+//         accept: 'application/vnd.github.machine-man-preview+json',
+//     },
+// });
 
-const installationId = data.id;
+// const installationId = data.id;
 
-const installationAccessToken = await getInstallationAccessTokenByInstallationId(installationId);
-  return installationAccessToken;
-};
+// const installationAccessToken = await getInstallationAccessTokenByInstallationId(installationId);
+//   return installationAccessToken;
+// };
 
-const getInstallationClient = async (owner, repo) => {
-    const installationAccessToken = await getInstallationAccessToken(owner, repo);
-    return new Octokit({
-    auth() {
-        return `token ${installationAccessToken}`;
-    },
-    });
-};
+// const getInstallationClient = async (owner, repo) => {
+//     const installationAccessToken = await getInstallationAccessToken(owner, repo);
+//     return new Octokit({
+//     auth() {
+//         return `token ${installationAccessToken}`;
+//     },
+//     });
+// };
 
 
 // const octokit = new Octokit({
@@ -78,6 +78,12 @@ webhooks.on('*', async ({ id, name, payload }) => {
       });
 })
 
-const create_check_run = (installationClient, params) => {
-    console.log('params', params)
-}
+const create_check_run = async ({owner, repo, name, head_sha}) => await request(`POST /repos/${owner}/${repo}/check-runs`,{
+    accept: 'application/vnd.github.antiope-preview+json',
+    name: 'Audit',
+    head_sha
+})
+
+webhooks.on('check_run', ({id,name,payload}) => {
+    console.log('check_run rcvddd.')
+})
