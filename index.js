20f8604dd45cfb4d98c9755e393e5db7d941c865
itadmin <itadmin@lapr101.successivetech.com>
2019-10-20 15:46:11 +0530

    index.js

--- index.js
+++ index.js
@@ -20,36 +20,37 @@ source.onmessage = (event) => {
   }).catch(console.error)
 }
 
-webhooks.on('*', async ({ id, name, payload }) => {
-    console.log(name, 'event received')
-    if(name === "check_suite") {
-        let owner = payload.repository.name;
-        let repo = payload.repository.full_name;
-        let repoName = "Audit";
-        let head_sha = payload.check_suite.head_sha
-        if(payload.action === "requested" || payload.action === "rerequested") {
-            console.log('here')
-            create({owner,repo,name:repoName,head_sha})
-        }
-        else {
-            let owner = payload.repository.owner.name;
-            let repo = payload.repository.full_name;
-            let check_run_id = payload.check_run.id;
-            octokit.checks.update({
-                owner,
-                repo,
-                check_run_id
-            })
-        }
+
+webhooks.on('check_suite', async({ id, name, payload }) => {
+    console.log(event, 'event recieved')
+})
+
+// webhooks.on('*', async ({ id, name, payload }) => {
+//     console.log(name, 'event received')
+//     if(name === "check_suite") {
+//         let owner = payload.repository.name;
+//         let repo = payload.repository.full_name;
+//         let repoName = "Audit";
+//         let head_sha = payload.check_suite.head_sha
+//         if(payload.action === "requested" || payload.action === "rerequested") {
+//             console.log('here')
+//             create({owner,repo,name:repoName,head_sha})
+//         }
+//         else {
+//             let owner = payload.repository.owner.name;
+//             let repo = payload.repository.full_name;
+//             let check_run_id = payload.check_run.id;
+//             octokit.checks.update({
+//                 owner,
+//                 repo,
+//                 check_run_id
+//             })
+//         }
         
-    }
-})
+//     }
+// })
 
-webhooks.on('check_suite', async({id, name, payload}) => {
-    console.log('check_run created')
-})
-
-const create = (params) => {
-    console.log('params', params)
-    octokit.checks.create(params)
-}
+// const create = (params) => {
+//     console.log('params', params)
+//     octokit.checks.create(params)
+// }
