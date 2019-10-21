52be1b784b66b18b50814b20be8ec3a6cf9b150c
itadmin <itadmin@lapr101.successivetech.com>
2019-10-21 13:29:44 +0530

    index.js

--- index.js
+++ index.js
@@ -3,49 +3,43 @@ const WebhooksApi = require('@octokit/webhooks')
 const Octokit = require("@octokit/rest")
 const request = require("@octokit/request")
 const { App } = require("@octokit/app")
-// const request = require('request');
 
-const webhooks = new WebhooksApi({
-  secret: 'pass'
-})
+const app = new App({
+    id: process.env.GITHUB_APP_IDENTIFIER,
+    privateKey: process.env.GITHUB_PRIVATE_KEY
+});
 
 const octokit = new Octokit({
-    secret: 'pass'
-})
+    async auth() {
+        const installationAccessToken = await app.getInstallationAccessToken({
+            installationId: process.env.INSTALLATION_ID
+        });
+        return `token ${installationAccessToken}`;
+    }
+});
+
+const webhooks = new WebhooksApi({ secret: 'pass'});
 
 const webhookProxyUrl = 'https://smee.io/cPuF5CJ9D3lTauuk'
 const source = new EventSource(webhookProxyUrl)
 source.onmessage = (event) => {
-  const webhookEvent = JSON.parse(event.data)
-  webhooks.verifyAndReceive({
-    id: webhookEvent['x-request-id'],
-    name: webhookEvent['x-github-event'],
-    signature: webhookEvent['x-hub-signature'],
-    payload: webhookEvent.body
-  }).catch(error => {
-      console.log('error', error)
-      Promise.reject()
-  })
+    const webhookEvent = JSON.parse(event.data)
+    webhooks.verifyAndReceive({
+        id: webhookEvent['x-request-id'],
+        name: webhookEvent['x-github-event'],
+        signature: webhookEvent['x-hub-signature'],
+        payload: webhookEvent.body
+    }).catch(error => {
+        console.log('error', error)
+        Promise.reject()
+    })
 }
 
-webhooks.on('check_suite', async ({ id, name, payload }) => {
-    console.log(name, 'event receivedd')
+webhooks.on('check_suite', async ({id,name,payload}) => {
     let owner = payload.repository.name;
     let repo = payload.repository.full_name;
     let repoName = "Audit";
     let head_sha = payload.check_suite.head_sha
-    octokit.request({
-        method: 'POST',
-        url: '/repos/:owner/:repo/check-run',
-        headers: {
-            accept: 'application/vnd.github.antiope-preview+json',
-        },
-        owner,
-        repo,
-        head_sha,
-    });
-})
 
-webhooks.on('check_run', ({id,name,payload}) => {
-    console.log(name, 'check_run rcvddd.')
+    octokit.checks.create({owner, repo, name: repoName, head_sha})
 })
