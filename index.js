62d9ede07ba98c73deb8ff671bc13d6317651945
itadmin <itadmin@lapr101.successivetech.com>
2019-10-21 16:49:49 +0530

    index.js

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
 const EventSource = require('eventsource');
 const WebhooksApi = require('@octokit/webhooks')
-const EventHandler = require('@octokit/webhooks/event-handler')
+const Octokit = require("@octokit/rest");
 
-const webhooks = new WebhooksApi({ secret: 'pass'});
+const octokit =  new Octokit({ auth: { username: "octocat", password: "secret"}});
+
+const webhooks = new WebhooksApi({
+  secret: 'pass'
+})
 
 const webhookProxyUrl = 'https://smee.io/cPuF5CJ9D3lTauuk'
 const source = new EventSource(webhookProxyUrl)
 source.onmessage = (event) => {
-    const webhookEvent = JSON.parse(event.data)
-    webhooks.verifyAndReceive({
-        id: webhookEvent['x-request-id'],
-        name: webhookEvent['x-github-event'],
-        signature: webhookEvent['x-hub-signature'],
-        payload: webhookEvent.body
-    }).catch(console.error)
+  const webhookEvent = JSON.parse(event.data)
+  webhooks.verifyAndReceive({
+    id: webhookEvent['x-request-id'],
+    name: webhookEvent['x-github-event'],
+    signature: webhookEvent['x-hub-signature'],
+    payload: webhookEvent.body
+  }).catch(console.error)
 }
 
-webhooks.on('check_suite', async ({ id, name, payload }) => {
-    let owner = payload.repository.name;
-    let repo = payload.repository.full_name;
-    let repoName = "Audit";
-    let head_sha = payload.check_suite.head_sha
-
-    if(payload.action == 'requested' || payload.action == 'rerequested') {
-        console.log('iffff')
-        const EventHandler = require('@octokit/webhooks/event-handler')
-        const eventHandler = new EventHandler({
-        async transform (event) {
-            // optionally transform passed event before handlers are called
-            return event
-        }
-        })
-        eventHandler.on('installation', asyncInstallationHook)
-
-        // put this inside your webhooks route handler
-        eventHandler.receive({
-            id: request.headers['x-github-delivery'],
-            name: request.headers['x-github-event'],
-            payload: request.body
-        }).catch(handleErrorsFromHooks)
+webhooks.on('*', async ({ id, name, payload }) => {
+  if(name === "check_suite") {
+    if(payload.action === "requested" || payload.action === "rerequested") {
+        console.log('if')
+      let owner = payload.repository.name;
+      let repo = payload.repository.full_name;
+      let repoName = "HexaKit AI";
+      let head_sha = payload.check_suite.head_sha
+      const data = octokit.checks.create({
+        owner,
+        repo,
+        name:repoName,
+        head_sha
+      })
+      console.log('data', data)
     }
+    else {
+      console.log('else')
+    }
+    
+  }
 })
