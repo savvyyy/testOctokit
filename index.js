24e47b31d730a0fee2a0c28573da5fa3d93c70c6
itadmin <itadmin@lapr101.successivetech.com>
2019-10-21 13:53:05 +0530

    index.js

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
 const EventSource = require('eventsource');
 const WebhooksApi = require('@octokit/webhooks')
-const Octokit = require("@octokit/rest")
-const request = require("@octokit/request")
-const { App } = require("@octokit/app")
-
-const app = new App({
-    id: process.env.GITHUB_APP_IDENTIFIER,
-    privateKey: process.env.GITHUB_PRIVATE_KEY
-});
-
-const octokit = new Octokit({
-    async auth() {
-        const installationAccessToken = await app.getInstallationAccessToken({
-            installationId: process.env.INSTALLATION_ID
-        });
-        console.log('installationAccessToken', installationAccessToken)
-        return `token ${installationAccessToken}`;
-    }
-});
 
 const webhooks = new WebhooksApi({ secret: 'pass'});
 
@@ -33,25 +15,13 @@ source.onmessage = (event) => {
     }).catch(console.error)
 }
 
-webhooks.on('check_suite', async ({id,name,payload}) => {
+webhooks.on('check_suite', async ({ id, name, payload }) => {
     let owner = payload.repository.name;
     let repo = payload.repository.full_name;
     let repoName = "Audit";
     let head_sha = payload.check_suite.head_sha
 
     if(payload.action == 'requested' || payload.action == 'rerequested') {
-        console.log('ifff')
-        octokit.request({
-            method: 'POST',
-            // https://github.com/octokit/rest.js/issues/862
-            url: '/repos/:owner/:repo/check-runs',
-            headers: {
-                accept: 'application/vnd.github.antiope-preview+json',
-            },
-            owner,
-            repo,
-            name: repoName,
-            head_sha,
-        });
+        console.log('if')
     }
 })
